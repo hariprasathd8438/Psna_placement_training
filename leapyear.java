21340cee9dcadd7df3bf9bb2b67364b50888cb4d
hariprasathd8438 <161866930+hariprasathd8438@users.noreply.github.com>
2025-09-02 14:23:44 +0530

    Create leapyear.java

---
+++ leapyear.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        if(a%400==0)
+            System.out.println("Leap year");
+        else if(a%100==0)
+            System.out.println("Not a Leap year");
+        else if(a%4==0)
+            System.out.println("Leap year");
+        else
+             System.out.println("Not a Leap year");
+            
+        
+    }
+}
