4d6072dbf429c83a01c5ba62822e1e1c4ff71377
hariprasathd8438 <161866930+hariprasathd8438@users.noreply.github.com>
2025-09-02 14:20:05 +0530

    Create number+or-.java

---
+++ number+or-.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        if(a>0)
+        System.out.println("Positive");
+        else if(a<0)
+         System.out.println("Negative");
+         else
+          System.out.println("Zero");
+        
+    }
+}
