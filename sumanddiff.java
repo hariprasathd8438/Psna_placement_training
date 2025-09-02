12b291ed8b1ee9cbace81ebf6d831e74814982d4
hariprasathd8438 <161866930+hariprasathd8438@users.noreply.github.com>
2025-09-02 14:56:57 +0530

    Create sumanddiff.java

---
+++ sumanddiff.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        int sum1=sc.nextInt();
+         int sum2=sc.nextInt();
+        float f1=sc.nextFloat();
+          float f2=sc.nextFloat();
+        int add=sum1+sum2;
+        int diff=sum1-sum2;
+        float add2=f1+f2;
+        float diff2=f1-f2;
+        System.out.println(add+" "+diff);
+           System.out.printf(add2+" "+diff2);
+        
+        
+      
+    }
+}
