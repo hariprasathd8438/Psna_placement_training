82b7de14f260c25b40b7dcf8fdee6e02934ebdb6
hariprasathd8438 <161866930+hariprasathd8438@users.noreply.github.com>
2025-09-02 14:18:48 +0530

    Create addeven.java

---
+++ addeven.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+      Scanner sc=new Scanner(System.in);
+      int a;
+      a=sc.nextInt();
+      
+      if(a%2==0)
+      System.out.println("EVEN");
+      else
+      System.out.println("ODD");
+    }
+}
